a7561fdef89f2c3e4e9fa8c7dd2e98ead89fb05d
Rafay Chaudhary <rchaudhary@augusta.edu>
2021-06-17 16:54:45 -0500

    Print to JSON File

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,7 @@
 from nba_api.stats import endpoints
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static import players
+import json
 
 # Get all players.
 player_dict = players.get_players()
@@ -9,7 +10,13 @@ player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
 
 player = player_info.common_player_info.get_json()
 
+if len(player) >= 0:
+        with open('Player.json', 'w') as outfile:
+            json.dump(player, outfile, indent=4)
+			
+
 print(player)
+
 # tryna get 
 # data = endpoints.__init__()
 
