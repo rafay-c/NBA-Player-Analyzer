f8222e3cde9b941e6bfb07583a25665a3401a826
Amine Sellak <asellak@augusta.edu>
2021-06-19 14:43:01 -0400

    able to pull first and last names

--- Scraper.py
+++ Scraper.py
@@ -8,25 +8,35 @@ player_dict = players.get_players()
 
 # Get a specific player's info
 player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+#gets json of players info
+player_json = player_info.common_player_info.get_json()
+#gets dictionary of players info
+player_dict = player_info.common_player_info.get_dict()
+#getting data from the dictionary
+for p in player_dict['data']:
+    print('First name:' + p[1]) #prints first name
+    print('Last name:' + p[2])
 
-player = player_info.common_player_info.get_json()
-
-
-if len(player) >= 0: #overwrites eachtime for some reason?
+#writes to json file(not sure if necessary here)  
+if len(player_json) >= 0: #overwrites eachtime for some reason?
         with open('Player.json', 'w') as outfile:
-            json.dump(player, outfile, indent=4)
+            json.dump(player_json, outfile, indent=4)
             outfile.write('\n')
 
+ 
+
+
 #need to find a way to only get first/last name, position, and calculate age
-with open('Player.json') as json_file:
-    data = json.load(json_file)
-    #for p in data['data']: 
-    print(data)
+#idk if this part is still needed, maybe when we export the data itll have to be in javascript/we can talk bout it
+#with open('Player.json') as json_file:
+    #data = json.load(json_file)
+    #for p in data: 
+        #print(p[0])
 
 
 
 
-# tryna get 
+# this shit might be useless
 # data = endpoints.__init__()
 
 # data = endpoints.CommonAllPlayers.get_data_frames(player_id=78630)
